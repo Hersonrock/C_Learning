e33a29d9431bc1cd5bed6bc1389a55fe2cba713b
Hersonrock <hjpg20@gmail.com>
2023-06-17 18:24:50 -0500

    adds argc and argv tests to ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -7,6 +7,13 @@ int main(int argc, char *argv[])
 
     // this is also a comment
     printf("You are %d miles away.\n", distance);
+    //here I will be making a set of outputs to test the utility of argc "Argument count" and argv "Argument Vector"
+    //argc will count the arguments the program was run with, including the program name
+    //argv will hold in the array the arguments, where argv[0] is the program name 
+    printf("The number of arguments is %d\n", argc);
+    for(int i=0;i<argc;i++){
+        printf("Argument %d: %s\n",i,argv[i]);
+    }
 
     return 0;
 }
