9322302bf0eddffabe85b23e695868df9c8f29d5
Hersonrock <hjpg20@gmail.com>
2023-06-17 20:52:41 -0500

    adds a littel floating precision usage for printf and ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -4,9 +4,11 @@ int main(int argc, char *argv[])
 {
     int age = 100;
     int height = 72;
+    float weight = 85.3;
 
     printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
 
+    printf("I weight %.2fkg\n", weight);
     return 0;
 }
