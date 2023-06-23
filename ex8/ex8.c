27e8198838307aaf8484c339987cb0780f2669fa
Hersonrock <hjpg20@gmail.com>
2023-06-23 18:41:05 -0500

    Day8, excersise completed

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -2,5 +2,18 @@
 
 int main(int argc, char *argv[]){
 
+if(argc==1){
+    printf("You only have one argument. You suck\n");
+}else if(argc > 1 && argc < 4){
+    printf("You have %d arguments\n",argc);
+    printf("Here are your arguments:\n");
+    for(int i=0;i<argc;i++){
+        printf("%s ",argv[i]);
+    }
+    printf("\n");
+}else{
+    printf("You have too many arguments. You suck\n");
+}
+
 }
 
